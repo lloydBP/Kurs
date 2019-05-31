702c7131988e62e80a2602b4a1ebccf587281fd8
lloydBP <pe4enko1919@gmail.com>
2019-05-31 23:37:26 +0300

    Созданы новые классы в папке Config

---
+++ src/java/config/Const.java
@@ -0,0 +1,13 @@
+package config;
+
+public class Const {
+
+    public static final String HOUSE_TABLE = "houses";
+
+    public static final String HOUSE_ID = "ID";
+    public static final String HOUSE_DISTRICT = "DISTRICT";
+    public static final String HOUSE_STREET = "STREET";
+    public static final String HOUSE_NUMBER = "NUMBER";
+    public static final String HOUSE_FLAT = "FLAT";
+    public static final String HOUSE_MEMBER = "MEMBER";
+}
---
+++ src/java/config/DatabaseHandler.java
@@ -0,0 +1,28 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseHandler extends Configs {
+
+    Connection conn;
+
+    public Connection getDbConnection()
+            throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql//" + dbHost + ":"
+                + dbPort + "/" + dbName;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Properties properties = new Properties();
+        properties.setProperty("user",dbUser);
+        properties.setProperty("password",dbPass);
+        properties.setProperty("useUnicode","true");
+        properties.setProperty("characterEncoding","cp1251");
+        conn = DriverManager.getConnection(url,properties);
+
+        return conn;
+    }
+}
